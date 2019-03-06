c3cdb47e2418915bf1a46b458ebd72327ff947dc
Igor Santos <igor.santos@vilt-group.com>
2019-03-06 16:22:32 -0300

    dev

--- src/js/main.js
+++ src/js/main.js
@@ -38,4 +38,43 @@ $(document).ready(function() {
 	  $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 800, 'linear');
 	});
 
+  // se email foi enviado
+
+  var baseUrl = window.location.hash;
+  var id = (baseUrl.substring(baseUrl.lastIndexOf('#') + 1)); 
+  if (id != '' && id == 'enviado'){
+    $('.alert-warning').removeClass('d-none');
+    $("html, body").animate({scrollTop: $('.vb-contato').offset().top - 80}, 500);
+  }
+
 });
+
+function Contato(){
+
+  d = document.form;
+  erro = false;
+  vbElement = '';
+              
+  if($('#name').val()==""){
+    erro=true; 
+    vbElement += '#name, ';
+  }
+  if($('#email').val()==""){
+    erro=true;
+    vbElement += '#email, ';
+  }
+  if($('#message').val()==""){
+    erro=true; 
+    vbElement += '#message';
+  }
+  if (erro) {
+    $(vbElement).addClass('vb-error');
+  } else {
+    $('form a').html('ENVIANDO...');
+    d.submit();
+  }
+}
+
+function vbFocusIpt(vb) {
+  $(vb).removeClass('vb-error');
+}
